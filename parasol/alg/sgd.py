8a50c3e92bfc89bdc9960de01879ef341425c7a5
xunzhang <xunzhangthu@gmail.com>
2013-10-10 12:03:21 +0800

    sgd.py added(test version).

---
+++ parasol/alg/sgd.py
@@ -0,0 +1,51 @@
+# Stochastic Gradient Descend
+
+import numpy as np
+from parasol.ps import paralg
+#from parasol.utils.parallel import npfactx
+
+class sgd(paralg):
+  
+    def __init__(self, comm, host_dict_lst, nworker, k, input_filename, output, alpha = 0.002, beta = 0.1, rounds = 3):
+    	paralg.__init__(self, comm, hosts_dict_lst)
+	self.rank = self.comm.Get_rank()
+	#self.a, self.b = npfactx(nworker)
+	self.k = k
+	self.filename = input_filename
+	self.output = output
+
+	self.alpha = alpha
+	self.beta = beta
+	self.rounds = rounds
+	# create folder
+	paralg.crt_outfolder(self, self.output)
+
+    def loss_func_gra(self, x, theta):
+	term = e ** (np.dot(x, theta))
+	return term / (1. + term)
+ 
+    def __sgd_kernel(self): #sample, label, max_iter = 20):
+	import random
+	m, n = self.sample.shape
+	self.theta = np.random.rand(n)
+	z = np.arange(m)
+	for it in xrange(self.max_iter):
+	    # shuffle indics
+	    random.shuffle(z)
+	    # traverse samples
+	    for i in z:
+		# before calc, pull theta first
+		# update weights
+		delta = self.alpha * (self.label[i] - self.loss_func_gra(self.sample[i], self.theta)) * self.sample[i] + 2. * self.beta * self.alpha * self.theta
+	        # push delta
+		theta += delta
+		
+    def solve(self):
+	from sklearn import datasets
+    	#from parasol.utils.lineparser import parser_b
+	#paralg.loadinput(self, self.filename, parser_b('\t'))
+	self.sample, self.label = datasets.make_classification(100, self.k)
+	self.sample = np.hstack((np.ones((self.sample.shape[0], 1)), self.sample))	
+	self.comm.barrier()
+	self.__sgd_kernel()
+	self.comm.barrier()
---
+++ tests/sgd/run_sgd.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python
+
+if __name__ == '__main__':
+    import json
+    from mpi4py import MPI
+    from optparse import OptionParser
+    from parasol.alg.sgd import sgd
+    from parasol.utils.gethostnames import get_hostnames_dict
+    comm = MPI.COMM_WORLD
+
+    optpar = OptionParser()
+    optpar.add_option('--hostsname', action = 'store', type = 'string', dest = 'hosts_name_st', help = 'hosts name string of parasol servers', metavar = 'dwalin:8888parasolbalin:7777')
+    options, args = optpar.parse_args()
+    hosts_dict_lst = get_hostnames_dict(options.hosts_name_st)
+
+    cfg_file = '/mfs/user/wuhong/release/parasol/config/sgd_cfg.json'
+    json_obj = json.loads(open(cfg_file).read())
+    nsrv = json_obj['nserver']
+    nworker = json_obj['nworker']
+    k = json_obj['k']
+    input_filename = json_obj['input']
+    output = json_obj['output']
+
+    alpha = json_obj['alpha']
+    beta = json_obj['beta']
+    rounds = json_obj['rounds'] 
+    
+    sgd_solver = sgd(comm, hosts_dict_lst, nworker, k, input_filename, output, alpha, beta, rounds)
+    sgd_solver.solve()
+    print sgd_solver.theta
